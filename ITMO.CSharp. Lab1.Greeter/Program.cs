92db875e8e86cf9ce58ba869b9e4f41b92a66349
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-04-26 19:16:12 +0300

    Добавьте файлы проекта.

---
+++ ITMO.CSharp. Lab1.Greeter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ITMO.CSharp._Lab1.Greeter
+{
+    class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello {0}", myName);
+        }
+    }
+}
