04755f0e37d6d4be008eb14f7a12f86059fff690
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-04-26 19:26:19 +0300

    11

---
+++ ITMO.CSharp.Lab1.DivideIt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ITMO.CSharp.Lab1.DivideIt
+{
+    class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the first integer");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+
+                Console.WriteLine("Please enter the second integer");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+
+                int k = i / j;
+
+                Console.WriteLine("The result of dividing {0} by {1} is {2}", i, j, k);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception was thrown: {0}", e);
+            }
+        }
+    }
+}
--- ITMO.CSharp/Program.cs
+++ ITMO.CSharp/Program.cs
@@ -2,29 +2,11 @@
 
 namespace ITMO.CSharp
 {
-    class DivideIt
+    class EMPTY
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("Please enter the first integer");
-                string temp = Console.ReadLine();
-                int i = Int32.Parse(temp);
-
-                Console.WriteLine("Please enter the second integer");
-                temp = Console.ReadLine();
-                int j = Int32.Parse(temp);
-
-                int k = i / j;
-
-                Console.WriteLine("The result of dividing {0} by {1} is {2}", i, j, k);
-            }
-
-            catch (Exception e)
-            {
-                Console.WriteLine("An exception was thrown: {0}", e);
-            }
+            
         }
     }
 }
