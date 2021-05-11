180ed8ee6351c152260be9a99fca49ac3d3b1ea6
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-05-11 09:26:54 +0300

    lab 4

---
+++ ITMO.CSharp.Lab4.Utility/Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab4.Utility
+{
+    
+    /// <summary>
+    ///   This is the test harness
+    /// </summary>
+
+    public class Test
+    {
+        public static void Main()
+        {
+            Console.Write("Enter first integer: ");
+            int x = int.Parse(Console.ReadLine());               // запрос ввода первого значения
+            Console.Write("Enter second integer: ");
+            int y = int.Parse(Console.ReadLine());               // запрос ввода второго значения
+            int greater = Utils.Greater(x, y);                   // вызов метода Greater  
+            Console.WriteLine("The greater value: {0}", greater);    // вывод в консоль большего числа
+
+            Console.WriteLine();   // разделение между функциями
+
+            Console.WriteLine("Before swap: x:{0}, y:{1}", x, y);
+            Utils.Swap(ref x, ref y);                               // вызов метода Swap
+            Console.WriteLine("After swap: x:{0}, y:{1}", x, y);
+
+            Console.WriteLine();   // разделение между функциями
+
+            int factorial;
+            Console.Write("Enter integer for factorial: ");
+            int z = int.Parse(Console.ReadLine());          //запрос ввода
+            bool ok = Utils.Factorial(z, out factorial);
+            if(ok)                                          //проверка выполнения и вывод результата
+            {
+                Console.WriteLine("Factorial {0} equal to {1}", z, factorial);
+            }
+            else
+            {
+                Console.WriteLine("I can't calculate the factorial of the number {0}", z);
+            }
+        }
+    }
+}
---
+++ ITMO.CSharp.Lab4.Utility/Utils.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ITMO.CSharp.Lab4.Utility
+{
+    
+    public class Utils
+    {
+        public static int Greater(int a, int b) //Возвращает большее число из 2-х
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        public static void Swap(ref int a, ref int b) // Меняет местами 2 числа
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static bool Factorial(int n, out int answer) //расчет факториала
+        {
+            int result = 1;
+            bool check = true;
+            if (n > 0)          //если положительное число считаем факториал
+            {
+                try
+                {
+                    checked         //проверка на переполнение
+                    {
+                        for (int i = 1; i <= n; i++)
+                        {
+                            result = result * i;
+                        }
+                    }
+                }
+                catch (System.OverflowException)
+                {
+                    result = 0;
+                    check = false;
+                }
+            }
+            else if(n == 0)        //если "0" возвращаем "0"
+            {
+                result = 1;
+            }
+            else                    //все остальное ошибка
+            {
+                check = false;
+            }
+            answer = result; //запись результата
+            return check;
+        }
+    }
+}
