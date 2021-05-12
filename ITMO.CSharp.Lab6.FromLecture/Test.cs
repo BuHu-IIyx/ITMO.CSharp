d83d28566a87b911451e3e445ae3c3059b2bb7e6
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-05-12 20:51:11 +0300

    FromLecture (1)

---
+++ ITMO.CSharp.Lab6.FromLecture/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab6.FromLecture
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            Triangle Treugolnik = new Triangle();
+            Treugolnik.a = 10;
+            Treugolnik.b = 5;
+            Treugolnik.c = 6;
+            Treugolnik.PrintParametrs();
+
+        }
+    }
+}
---
+++ ITMO.CSharp.Lab6.FromLecture/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ITMO.CSharp.Lab6.FromLecture
+{
+    class Triangle
+    {
+        public double a, b, c;
+        
+        public double Perimeter ()
+        {
+            double perimeter = a + b + c;
+            return perimeter;            
+        }
+        
+        public double Area ()
+        {
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+
+        public void PrintParametrs ()
+        {
+            Console.WriteLine("Side A = {0}", a);
+            Console.WriteLine("Side B = {0}", b);
+            Console.WriteLine("Side C = {0}", c);
+            Console.WriteLine("Perimeter = {0}", Perimeter());
+            Console.WriteLine("Area = {0}", Area());
+        }
+    }
+}
