2f53de9c75da9ecdaee64e6c4f017256c5a10400
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-05-13 22:49:42 +0300

    Lab6.Ex1

---
+++ ITMO.CSharp.Lab6.Ex1.CreateAccount/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ITMO.CSharp.Lab6.CreateAccount
+{
+    class BankAccount
+    {
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+
+        public void Populate(long accNo, decimal accBal)
+        {
+            this.accNo = accNo;
+            this.accBal = accBal;
+            accType = AccountType.Checking;
+        }
+
+        public long Number()
+        {
+            return accNo;
+        }
+
+        public decimal Balance()
+        {
+            return accBal;
+        }
+
+        public string Type()
+        {
+            string type = accType.ToString();
+            return type;
+        }
+    }
+}
---
+++ ITMO.CSharp.Lab6.Ex1.CreateAccount/CreateAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab6.CreateAccount
+{
+    class CreateAccount
+    {
+        static void Main()
+        {
+            BankAccount berts = NewBankAccount();
+            Write(berts);
+
+            BankAccount freds = NewBankAccount();
+            Write(freds);
+        }
+
+        static BankAccount NewBankAccount()
+        {
+            BankAccount created = new BankAccount();
+
+            Console.Write("Enter the account number: ");
+            long number = long.Parse(Console.ReadLine());
+
+            Console.Write("Enter the account balance: ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+
+            //created.accNo = number;
+            //created.accBal = balance;
+            //created.accType = AccountType.Checking;
+            
+            created.Populate(number, balance);
+
+            return created;
+        }
+
+        static void Write(BankAccount toWrite)
+        {
+            Console.WriteLine("Account number is {0}", toWrite.Number());
+            Console.WriteLine("Account balance is {0}", toWrite.Balance());
+            Console.WriteLine("Account type is {0}", toWrite.Type());
+        }
+    }
+}
