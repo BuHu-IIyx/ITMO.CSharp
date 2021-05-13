817f866599292010f4717f9899742ddee0ee7836
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-05-14 01:48:49 +0300

    Lab7.Ex2

---
+++ ITMO.CSharp.Lab7.Ex2.Utils/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ITMO.CSharp.Lab7.Ex2.Utils
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string someString;
+            Console.WriteLine("Type text for reverse: ");
+            someString = Console.ReadLine();
+
+            Utils.Reverse(ref someString);
+
+            Console.WriteLine(someString);
+        }
+    }
+}
