ce122c9f7d82a97f5bc1be7408ed46550316c2b3
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-05-13 22:50:06 +0300

    Lab6.Ex2

---
+++ ITMO.CSharp.Lab6.Ex2.UniqueNumbers/CreateAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ITMO.CSharp.Lab6.UniqueNumbers
+{
+    class CreateAccount
+    {
+        static void Main()
+        {
+            BankAccount berts = NewBankAccount();
+            Write(berts);
+
+            BankAccount freds = NewBankAccount();
+            Write(freds);
+        }
+
+        static BankAccount NewBankAccount()
+        {
+            BankAccount created = new BankAccount();
+
+            //Console.Write("Enter the account number: ");
+            //long number = long.Parse(Console.ReadLine());
+            //long number = BankAccount.NextNumber();
+
+            Console.Write("Enter the account balance: ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+
+            created.Populate(balance);
+
+            return created;
+        }
+
+        static void Write(BankAccount toWrite)
+        {
+            Console.WriteLine("Account number is {0}", toWrite.Number());
+            Console.WriteLine("Account balance is {0}", toWrite.Balance());
+            Console.WriteLine("Account type is {0}", toWrite.Type());
+        }
+    }
+}
