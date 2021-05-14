7330c85c8fdc81a6b7637de34f006225ef297a3b
Alexander Vasilenko <buhu.iiyxs@gmail.com>
2021-05-14 03:25:28 +0300

    Lab7.Ex4

---
+++ ITMO.CSharp.Lab7.Ex4.InterfaceTest/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSharp.Lab7.Ex4.InterfaceTest
+{
+    /// <summary>
+    ///   This the test harness
+    /// </summary>
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            ulong ul = 0;
+            string s = "Test";
+
+            Console.WriteLine("int: {0}", Utils.IsItFormattable(i));
+            Console.WriteLine("ulong: {0}", Utils.IsItFormattable(ul));
+            Console.WriteLine("String: {0}", Utils.IsItFormattable(s));
+        }        
+    }
+}
